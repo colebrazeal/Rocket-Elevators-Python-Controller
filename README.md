2dd23306037778ce6048ca90dd20dfe466f44d3a
colebrazeal <cbrazeal17@gmail.com>
2022-06-17 16:01:15 -0400

    Updated

--- README.md
+++ README.md
@@ -1,34 +1,25 @@
 # Rocket-Elevators-Python-Controller
-This is the template to use for the python residential controller. You will find the classes that should be used along with some methods described in the requirements. The necessary file to run some tests is also included. 
 
-### Installation
+This program controls a Column of elevators.
 
-First, depending on your python version, make sure to install the Package Installer for Python (PIP) if needed:
+It sends an elevator when a user presses a button on a floor and it takes
+a user to its desired floor when a button is pressed from the inside the elevator
 
-https://pip.pypa.io/en/stable/installing/
+Elevator selection is based on the users location when requesting an elevator
 
-Next, install Pytest:
+# Dependencies
 
-https://docs.pytest.org/en/6.2.x/getting-started.html
+To be able to try the program, you need to install npm
 
-### Running the tests
+- Dependency #1: "pip" is needed to test the elevator controller and make sure the code is operating properly to function the elevators with the controller
 
-To launch the tests:
+# Usage
 
-`pytest`
+Used to operate elevators in a residential building for people transporting through out the building
 
-With a fully completed project, you should get an output like:
-
-![Screenshot from 2021-06-15 13-13-13](https://user-images.githubusercontent.com/28630658/122095645-a41fa000-cddb-11eb-9322-81a766cce4bb.png)
-
-You can also get more details about each test by adding the `-v` flag: 
-
-`pytest -v` 
-
-which should give something like: 
-
-![Screenshot from 2021-06-15 13-13-33](https://user-images.githubusercontent.com/28630658/122095759-c74a4f80-cddb-11eb-999d-dfe35dbe7d18.png)
-
-The test file can be left in your final project but no scenarios should be present in your code. The grader will run tests similar to the ones provided.
-
-Of course, make sure to edit this Readme file to describe your own project!
+## Example
+1. open terminal 
+2. you need to type "python get-pip.py" in your terminal
+3. cd to the location of the file in your terminal and make sure you are in the right location by typing "ls" 
+4. type "python3 -m pytest" to test the elevator controller
+5. should get a result saying "all tests passed"
--- residential_controller.py
+++ residential_controller.py
@@ -1,18 +1,208 @@
+# from contextlib import nullcontext
+
+
+elevatorID = 1
+floorRequestButtonID = 1
+callButtonID = 1
+
+ #----------------------------COLUMN----------------------------#
+
 class Column:
     def __init__(self, _id, _amountOfFloors, _amountOfElevators):
+        self.ID = _id
+        self.status = "0"
+        self.amountOfFloors = _amountOfFloors
+        self.amountOfFloors = _amountOfElevators
+        self.elevatorList = []
+        self.callButtonList = []
 
+        self.createElevators(_amountOfFloors, _amountOfElevators)
+        self.createCallButtons(_amountOfFloors)
+
+    #----------------------------METHODS----------------------------#
+
+    def createCallButtons(self, _amountOfFloors):
+        buttonFloor = 1
+        global callButtonID
+        for i in range(_amountOfFloors):
+            if(buttonFloor < _amountOfFloors):
+                callUpButton = CallButton(callButtonID, buttonFloor, "up")
+                self.callButtonList.append(callUpButton)
+                callButtonID += 1
+            
+    
+            if(buttonFloor > 1):
+                callDownButton = CallButton(callButtonID, buttonFloor, "down")
+                self.callButtonList.append(callDownButton)
+                callButtonID += 1
+            
+            buttonFloor += 1
+
+    def createElevators(self, _amountOfFloors, _amountOfElevators):
+        for i in range(_amountOfElevators): 
+            global elevatorID
+            elevator = Elevator(elevatorID, _amountOfFloors)
+            self.elevatorList.append(elevator)
+            elevatorID += 1  
+        
+    ##Simulate when a user press a button outside the elevator
+
+    def requestElevator(self, floor, direction):
+        elevator = self.findElevator(floor, direction)
+        elevator.floorRequestList.append(floor)
+        elevator.sortFloorList()
+        elevator.move()
+        elevator.operateDoors()
+    
+        return elevator
+    
+    ##We use a score system depending on the current elevators state. Since the bestScore and the referenceGap are 
+    ##higher values than what could be possibly calculated, the first elevator will always become the default bestElevator, 
+    ##before being compared with to other elevators. If two elevators get the same score, the nearest one is prioritized.
+
+    def findElevator(self, requestedFloor, requestedDirection):
+            bestElevatorInformations = {
+            "bestElevator": None,
+            "bestScore": 5,
+            "referenceGap": 10000000
+           }
+
+            for elevator in self.elevatorList:
+                ##The elevator is at my floor and going in the direction I want
+                if (requestedFloor == elevator.currentFloor and elevator.status == "stopped" and requestedDirection == elevator.direction): 
+                    bestElevatorInformations = self.checkIfElevatorIsBetter(1, elevator, bestElevatorInformations, requestedFloor)
+                    #The elevator is lower than me, is coming up and I want to go up
+                elif (requestedFloor > elevator.currentFloor and elevator.direction == "up" and requestedDirection == elevator.direction):
+                    bestElevatorInformations = self.checkIfElevatorIsBetter(2, elevator, bestElevatorInformations, requestedFloor)
+                    #The elevator is higher than me, is coming down and I want to go down
+                elif (requestedFloor < elevator.currentFloor and elevator.direction == "down" and requestedDirection == elevator.direction):
+                    bestElevatorInformations = self.checkIfElevatorIsBetter(2, elevator, bestElevatorInformations, requestedFloor)
+                    #The elevator is idle
+                elif (elevator.status == "idle"):
+                    bestElevatorInformations = self.checkIfElevatorIsBetter(3, elevator, bestElevatorInformations, requestedFloor)
+                    #The elevator is not available, but still could take the call if nothing better is found
+                else:
+                    bestElevatorInformations = self.checkIfElevatorIsBetter(4, elevator, bestElevatorInformations, requestedFloor)
+            
+                
+            
+            return bestElevatorInformations["bestElevator"]
+
+    def checkIfElevatorIsBetter(self, scoreToCheck, newElevator, bestElevatorInformations, floor):
+        if (scoreToCheck < bestElevatorInformations["bestScore"]):
+            bestElevatorInformations["bestScore"] = scoreToCheck
+            bestElevatorInformations["bestElevator"] = newElevator
+            bestElevatorInformations["referenceGap"] = abs(newElevator.currentFloor - floor)
+        elif (bestElevatorInformations["bestScore"] == scoreToCheck):
+            gap = abs(newElevator.currentFloor - floor)
+            if (bestElevatorInformations["referenceGap"] > gap):
+                bestElevatorInformations["bestScore"] = scoreToCheck
+                bestElevatorInformations["bestElevator"] = newElevator
+                bestElevatorInformations["referenceGap"] = gap
+                            
+        return bestElevatorInformations; 
+
+#----------------------------COLUMN----------------------------# 
+
+#----------------------------ELEVATOR----------------------------# 
 
 class Elevator:
-    def __init__(self, _id, _amountOfFloors):
+    def __init__(self, _id, _amountOfFloors,):
+        self.ID = _id
+        self.status = ""
+        self.amountOfFloors = _amountOfFloors
+        self.currentFloor = 1
+        self.direction = None 
+        self.door = Door(_id)
+        self.floorRequestButtonList = []
+        self.floorRequestList = []
+        self.createFloorRequestButton(_amountOfFloors)
 
+    def createFloorRequestButton(self, _amountOfFloors):
+        buttonFloor = 1
+        global floorRequestButtonID
+        for i in range(_amountOfFloors):
+            floorRequestButton = FloorRequestButton(floorRequestButtonID, buttonFloor)
+            self.floorRequestButtonList.append(floorRequestButton)
+            buttonFloor += 1
+            floorRequestButtonID += 1 
+        
+    #Simulate when a user press a button inside the elevator
+
+    def requestFloor(self, floor):
+        self.floorRequestList.append(floor)
+        self.sortFloorList()
+        self.move()
+        self.operateDoors()
+
+    def move(self):
+        global screenDisplay
+        while(len(self.floorRequestList)):
+            destination = self.floorRequestList[0]
+            screenDisplay = 0
+            self.status = "moving"
+            if(self.currentFloor < destination):
+                self.direction = "up"
+
+                while(self.currentFloor < destination):
+                    self.currentFloor += 1
+                    screenDisplay = self.currentFloor
+                
+            elif(self.currentFloor > destination):
+                self.direction = "down"
+
+                while self.currentFloor > destination:
+                    self.currentFloor -= 1
+                    screenDisplay = self.currentFloor
+                
+            
+            self.status = "stopped"
+            self.floorRequestList.pop()
+
+            
+        self.status = "idle"
+        
+    def sortFloorList(self):
+        if (self.direction == "up"):
+            self.floorRequestList.sort()
+        else: 
+            self.floorRequestList.sort(reverse = True)
+            
+
+    def operateDoors(self):
+        #self.door = Door()
+        self.door.status = "opened"
+    
+#----------------------------ELEVATOR----------------------------#      
+
+#----------------------------CALL BUTTON----------------------------#
 
 class CallButton:
     def __init__(self, _id, _floor, _direction):
+        self.ID = _id
+        self.floor = _floor
+        self.direction = _direction
+        self.status = ""
 
+#----------------------------CALL BUTTON----------------------------#
+
+#----------------------------FLOOR REQUEST BUTTON----------------------------#
 
 class FloorRequestButton:
     def __init__(self, _id, _floor):
+        self.ID = _id
+        self.floor = _floor
+        self.status = ""
 
+#----------------------------FLOOR REQUEST BUTTON----------------------------#
+
+#----------------------------DOOR----------------------------#
 
 class Door:
     def __init__(self, _id):
+        self.ID = _id
+        self.status = ""
+
+#----------------------------DOOR----------------------------#
+
+##module.exports = { Column, Elevator, CallButton, FloorRequestButton, Door }
